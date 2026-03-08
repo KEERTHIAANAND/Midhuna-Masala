0925e1489dcf04eeba3cc5166b468eca77363c8a
KEERTHIAANAND <keerthiaanand77@gmail.com>
2026-03-08 23:32:44 +0530

    feat: initialize Next.js project with foundational configuration and the HeroSection component.

--- midhunamasala/next.config.ts
+++ midhunamasala/next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     qualities: [75, 80, 90],
   },
+  allowedDevOrigins: [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+  ],
 };
 
 export default nextConfig;
